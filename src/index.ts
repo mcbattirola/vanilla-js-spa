f2769ebfc0ef8295662170e81728c8f6ffe55091
Matheus <mcbattirola@hotmail.com>
2020-06-21 20:34:49 -0300

    refactoring components as classes

--- src/index.ts
+++ src/index.ts
@@ -1,97 +1,43 @@
-import promiseXHR from "./promiseXHR";
-import UsersTable from "./usersTable";
+import { UsersTable, Renderizable } from "./usersTable";
 
-interface User {
-    id: number;
-    name: string;
-    login: string;
-    permissions: ModulePermissions[]
-}
-
-interface ModulePermissions {
-    moduleId: number;
-    moduleName: string;
-    permissions: Permission[];
-}
-
-interface Permission {
-    create: boolean;
-    read: boolean;
-    update: boolean;
-    delete: boolean;
-}
+let rootElement: HTMLElement;
+let currentContentBuilder: Renderizable;
 
 const initApp = async () => {
     try {
-        const rootElement = document.getElementById('app');
-        const mainContent = document.createElement("main");
-        mainContent.className = "app-container";
-
-        const userRequest = await promiseXHR("https://simple-users-backend.herokuapp.com/user", "GET");
-        const users: User[] = JSON.parse(userRequest.response);
-
-        mainContent.append(createUsersTable(users));
-        rootElement.appendChild(mainContent);
-
+        rootElement = document.getElementById('app');
+        changeComponentHandler(new UsersTable);
     } catch (error) {
 
     }
 };
 
-const createUsersTable = (users: User[]): HTMLElement => {
-    const table = document.createElement("table");
-    table.className = "app-table";
+const changeComponentHandler = (component: Renderizable) => {
+    currentContentBuilder = component;
+    component.changeComponentHandler = changeComponentHandler;
+    renderComponent(component);
+}
 
-    table.appendChild(createUsersTableHeader())
+const renderComponent = async (component: Renderizable) => {
+    const appContent = await currentContentBuilder.render();
+    const mainContainer = createMainContainer();
+    mainContainer.appendChild(appContent)
+    renderOnRoot(mainContainer);
+}
 
-    for (let index = 0; index < users.length; index++) {
-        const user: User = users[index];
-        table.appendChild(createUserTr(user));
+const createMainContainer = () : HTMLElement => {
+    const mainContainer = document.createElement("main");
+    mainContainer.className = "app-container";
+    return mainContainer;
+}
+
+const renderOnRoot = (component: HTMLElement) => {
+    if (rootElement) { 
+        if (rootElement.firstChild) {
+            rootElement.removeChild(rootElement.firstChild);
+        }
+        rootElement.appendChild(component);
     }
-
-    return table;
-};
-
-const createUsersTableHeader = () : HTMLElement => {
-    const header = document.createElement("tr");
-    header.className = "app-table-header";
-    
-    header.appendChild(createElementAndSetInnerHTML("th", "ID", "app-table-th"));
-    header.appendChild(createElementAndSetInnerHTML("th", "Name", "app-table-th"));
-    header.appendChild(createElementAndSetInnerHTML("th", "Login", "app-table-th"));
-
-    return header;
 }
-
-const createUserTr = (user: User): HTMLElement => {
-    const tr = document.createElement("tr");
-    tr.className = "app-table-tr";
-    tr.onclick = (event: MouseEvent) => HandleUserClick(user.id, event);
-
-    const idTd = createElementAndSetInnerHTML("td", user.id.toString(), "app-table-td");
-    tr.appendChild(idTd);
-
-    const nameTd = createElementAndSetInnerHTML("td", user.name, "app-table-td");
-    tr.appendChild(nameTd);
-
-    const loginTd = createElementAndSetInnerHTML("td", user.login, "app-table-td");
-    tr.appendChild(loginTd);
-
-    return tr;
-}
-
-const HandleUserClick = (userId: number, event: MouseEvent) => {
-    console.log('userId: ', userId)
-}
-
-const createElementAndSetInnerHTML = (tag: string, value: string, className?: string) : HTMLElement => {
-    const element = document.createElement(tag);
-    element.innerHTML = value;
-    if (className) {
-        element.className = className
-    }
-    return element;    
-}
-
 
 window.addEventListener("load", initApp)
--- src/usersTable.ts
+++ src/usersTable.ts
@@ -1,8 +1,102 @@
-interface Renderizable {
-    render(): HTMLElement;
+import promiseXHR from "./promiseXHR";
+
+export interface Renderizable {
+    changeComponentHandler: (component: Renderizable) => void;
+    render: () => HTMLElement | Promise<HTMLElement>;
 }
-export default class UsersTable implements Renderizable {
+
+class PermissionsTable implements Renderizable {
+    changeComponentHandler: (component: Renderizable) => void; 
     render() {
-        return new HTMLElement();
+        const table = document.createElement("table");
+        return table;
+    };
+}
+
+interface User {
+    id: number;
+    name: string;
+    login: string;
+    permissions: ModulePermissions[]
+}
+
+interface ModulePermissions {
+    moduleId: number;
+    moduleName: string;
+    permissions: Permission[];
+}
+
+interface Permission {
+    create: boolean;
+    read: boolean;
+    update: boolean;
+    delete: boolean;
+}
+
+export class UsersTable implements Renderizable {
+    changeComponentHandler: (component: Renderizable) => HTMLElement | Promise<HTMLElement>;
+    async render() {
+        const userRequest = await promiseXHR("https://simple-users-backend.herokuapp.com/user", "GET");
+        const users: User[] = JSON.parse(userRequest.response);
+        return this.createUsersTable(users);
+    };
+
+    createUsersTable(users: User[]): HTMLElement {
+        const table = document.createElement("table");
+        table.className = "app-table";
+
+        table.appendChild(this.createUsersTableHeader())
+
+        for (let index = 0; index < users.length; index++) {
+            const user: User = users[index];
+            table.appendChild(this.createUserTr(user));
+        }
+
+        return table;
+    };
+
+    createUsersTableHeader(): HTMLElement {
+        const header = document.createElement("tr");
+        header.className = "app-table-header";
+
+        header.appendChild(this.createElementAndSetInnerHTML("th", "ID", "app-table-th"));
+        header.appendChild(this.createElementAndSetInnerHTML("th", "Name", "app-table-th"));
+        header.appendChild(this.createElementAndSetInnerHTML("th", "Login", "app-table-th"));
+
+        return header;
     }
+
+    createUserTr(user: User): HTMLElement {
+        const tr = document.createElement("tr");
+        tr.className = "app-table-tr";
+        tr.onclick = (event: MouseEvent) => this.HandleUserClick(user.id, event);
+
+        const idTd = this.createElementAndSetInnerHTML("td", user.id.toString(), "app-table-td");
+        tr.appendChild(idTd);
+
+        const nameTd = this.createElementAndSetInnerHTML("td", user.name, "app-table-td");
+        tr.appendChild(nameTd);
+
+        const loginTd = this.createElementAndSetInnerHTML("td", user.login, "app-table-td");
+        tr.appendChild(loginTd);
+
+        return tr;
+    }
+
+    HandleUserClick(userId: number, event: MouseEvent) {
+        console.log('userId: ', userId)
+        // instancia tabela de permissoes
+        // passa os dados pra ela
+        this.changeComponentHandler(new PermissionsTable);
+    }
+
+    createElementAndSetInnerHTML(tag: string, value: string, className?: string): HTMLElement {
+        const element = document.createElement(tag);
+        element.innerHTML = value;
+        if (className) {
+            element.className = className
+        }
+        return element;
+    }
+
 }
